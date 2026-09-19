96c899710753f74bb17921c21a2da61226f3ff00
Renato <ren.barriga@duocuc.cl@duocuc.cl>
2026-09-19 02:57:22 -0300

    feat(catalog): add CreateGameDto and UpdateGameDto

---
+++ src/catalog/dto/create-game.dto.ts
@@ -0,0 +1,5 @@
+export class CreateGameDto {
+  nombre: string;
+  descripcion: string;
+  imagen?: string | null;
+}
---
+++ src/catalog/dto/update-game.dto.ts
@@ -0,0 +1,5 @@
+export class UpdateGameDto {
+  nombre?: string;
+  descripcion?: string;
+  imagen?: string | null;
+}
