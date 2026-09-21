a20ef9463674f23cf7831455611afe7379a07750
Renato <ren.barriga@duocuc.cl@duocuc.cl>
2026-09-21 00:48:58 -0300

    fix: quitar CORS del microservicio; CORS solo en el API Gateway (rubrica)

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,7 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  app.enableCors({
-    origin: process.env.CORS_ORIGIN ?? 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
-  
+
   app.useGlobalFilters(new HttpExceptionFilter());
   
   app.useGlobalPipes(
